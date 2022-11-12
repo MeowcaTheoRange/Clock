165d7daa0eef9a75cb22ac966b3c6e1282e592b8
MeowcaTheoRange <58280776+MeowcaTheoRange@users.noreply.github.com>
2022-11-11 20:43:16 -0600

    Update update.js

--- update.js
+++ update.js
@@ -1,26 +1,12 @@
-var prevSha = localStorage.getItem("commitsha") ?? "ThisShouldntBeASHA";
-
-setInterval(reqData, 60000);
-
-//balls lmao
-
-function reqData() {
+function reqData(start) {
   var donedata = (e) => {
     var resp = e;
 
-//     if (event.target.status.toString().includes("40")) {
-//       console.log(event.target.status + " error :[")
-//       return;
-//     }
-
     if (resp != prevSha) {
+      window.sessionStorage.setItem("commitsha", resp);
+      if (start) return;
       document.querySelector("#updatesha").innerHTML = `from ${prevSha} to ${resp}`;
-      if (prevSha == "ThisShouldntBeASHA") {
-        localStorage.setItem("commitsha", resp);
-      } else {
-        document.querySelector("#updatedialog").classList.add("open");
-        localStorage.setItem("commitsha", resp);
-      }
+      document.querySelector("#updatedialog").classList.add("open");
     }
   };
 
@@ -29,4 +15,6 @@ function reqData() {
   .then(y => donedata(y));
 }
 
-reqData();
+setInterval(reqData, 60000);
+
+reqData(true);
